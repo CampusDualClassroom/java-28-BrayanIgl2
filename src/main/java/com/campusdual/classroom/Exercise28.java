e0fe54bd0a3885966ce2830e9422b43e434cd824
Brayan Iglesias <iglesiasbrayan2@gmail.com>
2024-09-27 10:48:08 +0200

    28 ok

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,10 @@
 package com.campusdual.classroom;
 
 public class Exercise28 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        int n = 1/0;
+        }
 
     }
-}
+
